60512e45d18321fba1afa6b6e437603e26d79ca3
Radhika Agrawal <radhikaagrawal2403@gmail.com>
2025-12-29 01:40:19 +0530

    Fix Home.css import path to lowercase

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,7 +4,7 @@ function Home({ onStart }) {
   return (
     <div className="home">
       {/* Fullscreen background */}
-      <img id="bg" src="./Home.png" alt="Background" />
+      <img id="bg" src="/Home.png" alt="Background" />
 
       {/* Foreground content */}
       <h1>Photo Booth</h1>
